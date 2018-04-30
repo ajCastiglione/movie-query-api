e2c867efd0cd97435d581400ebd38b619f3da5a0
ajCastiglione <antoniocastiglione10@gmail.com>
2018-04-30 12:01:52 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# TMDb API Movie Query
+
+Simple web app to query TMDb's database to find some good movies to watch.
+
+## Dependencies
+<ol>
+<li>Gulp</li>
+<li>jQuery</li>
+<li>SCSS</li>
+</ol>
+
+## Future Plans
+
+<ul>
+<li>Make this app more visually appealing</li>
+<li>Implement a service worker and local storage to deliver content offline</li>
+<li>Cache previous request locally to show available queries while offline</li>
+</ul>
---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+
+gulp.task('default', function() {
+  gulp.watch('scss/**/*.scss', gulp.series('styles'));
+});
+
+gulp.task('styles', function() {
+  return gulp.src('scss/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(gulp.dest('./css'));
+});
+
+browserSync.init({
+  files: ['index.html', 'css/*.css', 'js/*.js'],
+  server: "./"
+});
+browserSync.stream();
