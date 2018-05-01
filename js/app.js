5096c65da2a807fe074726ecfc1343c91284f311
ajCastiglione <antoniocastiglione10@gmail.com>
2018-05-01 16:40:50 -0400

    updated query url to show only US based movies

--- js/app.js
+++ js/app.js
@@ -7,12 +7,17 @@
  */
 
 $(function () {
-    let discoverContainer = $(".discover-container"), searchContainer = $(".search-container");
-    let discoverOpts = $('.input-discover-options'), searchFields = $('.input-search-field'), selectedOption;
+    let discoverContainer = $(".discover-container"),
+        searchContainer = $(".search-container");
+    let discoverOpts = $('.input-discover-options'),
+        searchFields = $('.input-search-field'),
+        selectedOption;
+    let out = $(".output-section");
     let discoverUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&`;
     discoverOpts.hide();
-    let movieData = [];
-    
+    let movieData = [] || JSON.parse(storage.movieProp),
+        genresArr = [] || JSON.parse(storage.genreList);
+
 
     //showing search fields
     discoverContainer.on('click', e => {
@@ -25,30 +30,48 @@ $(function () {
     });
 
     //Grabbing the id of clicked btn
-    discoverOpts.on('click', '.discover-option', function() {
+    discoverOpts.on('click', '.discover-option', function () {
         param = $(this).attr('value');
-        if (param == "primary_release_date.gte=") {param = `${param}${dateStamp}`}
-        else if(param == "primary_release_year=") {param = `${param}${year}`}
+        if (param == "primary_release_date.gte=") {
+            param = `${param}${dateStamp}`
+        } else if (param == "primary_release_year=") {
+            param = `${param}${year}`
+        }
         getResults(param);
     });
 
     //Initializing search and waiting for it to return a response
     async function getResults(param) {
+        movieData = [];
         const Response = await fetchResults(param);
         const Genres = await fetchGenres();
 
-        for(movie of Response.results) {
+        for (genre of Genres.genres) {
+            genresArr.push({
+                'genre_id': genre.id,
+                'genre_name': genre.name
+            });
+        }
+
+        for (movie of Response.results) {
+            let movieGenres = await filterGenres(movie.genre_ids);
+            let poster = `${baseImgUrl}${movie.poster_path}`;
+
+            if (!movieGenres) movieGenres = `Not Listed`;
+            if (!movie.poster_path) poster = "https://d32qys9a6wm9no.cloudfront.net/images/movies/poster/500x735.png";
             movieData.push({
                 'id': movie.id,
-                'genres': movie.genre_ids,
+                'genres': movieGenres,
                 'title': movie.original_title,
-                'img': `${baseImgUrl}${movie.poster_path}`,
+                'img': poster,
                 'release': movie.release_date
             });
         }
 
-        storage = JSON.stringify(movieData);
-        console.log(movieData);
+        storage.movieProp = JSON.stringify(movieData);
+        storage.genreList = JSON.stringify(genresArr);
+
+        displayContent();
     }
 
     //Grabbing results based on search
@@ -56,13 +79,12 @@ $(function () {
         let result;
         try {
             result = await $.ajax({
-                url: `${discoverUrl}${param}`,
+                url: `${discoverUrl}${param}&region=US`,
                 type: 'GET'
             });
 
             return result;
-        }
-        catch (error) {
+        } catch (error) {
             console.error(error);
         }
     }
@@ -75,9 +97,43 @@ $(function () {
                 type: 'GET'
             });
             return result;
-        }
-        catch(error) {
+        } catch (error) {
             console.error(error);
+        }
+    }
+
+    async function filterGenres(ids) {
+        if (ids.length == 0) return;
+
+        let genreNames = "";
+        for (id of ids) {
+            for (genre of genresArr) {
+                if (id == genre.genre_id) {
+                    genreNames += `${genre.genre_name} `;
+                }
+            }
+        }
+        return genreNames;
+    }
+
+    function displayContent() {
+        out.empty();
+        let content;
+
+        for (movie of movieData) {
+            content = $(`
+            <div class="movie-result-content clearfix">
+                <div class="movie-image col-xs-6">
+                    <img src="${movie.img}" alt="${movie.title}">
+                </div>
+                <div class="movie-content col-xs-6">
+                    <h3>${movie.title}</h3>
+                    <p class="release-date">Release Date: <br> ${movie.release}</p>
+                    <p class="genre-list">Genres: <br> ${movie.genres}</p>
+                </div>
+            </div>
+        `);
+            out.append(content);
         }
     }
 
