8cdc4ccc28f9ca55c43b7d8d3602feb95c7c8f9b
ajCastiglione <antoniocastiglione10@gmail.com>
2018-05-01 10:07:48 -0400

    transferred reference urls to the main js file

---
+++ js/app.js
@@ -0,0 +1,7 @@
+/*
+* Reference URLs:
+* https://www.themoviedb.org/documentation/api?language=en - docs
+* https://developers.themoviedb.org/3/getting-started/introduction - docs in depth
+* https://api.themoviedb.org/3/{query-type}/movies/api_key={api-key} - example discover url
+* https://api.themoviedb.org/3/search/movie?api_key={api_key}&query=Jack+Reacher - example search url
+*/
