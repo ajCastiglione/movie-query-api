9267e7d5d7a6021b5833262bf516dbe41c917c0f
ajCastiglione <antoniocastiglione10@gmail.com>
2018-05-01 15:06:50 -0400

    added query functionality and obtain results. need to grab genres now and figure out the best way to associate with each result

--- js/app.js
+++ js/app.js
@@ -7,25 +7,56 @@
  */
 
 $(function () {
-    let discover = $(".discover-container"), search = $(".search-container");
-    let apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&sort_by=popularity.desc`;
+    let discoverContainer = $(".discover-container"), searchContainer = $(".search-container");
+    let discoverOpts = $('.input-discover-options'), searchFields = $('.input-search-field'), selectedOption;
+    let discoverUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&`;
+    discoverOpts.hide();
+    let movieData = [];
+    
 
     //showing search fields
-    discover.on('click', e => {
-        $('.input-search-field').hide();
-        $(".input-discover-options").fadeToggle(500);
+    discoverContainer.on('click', e => {
+        searchFields.hide();
+        discoverOpts.fadeToggle(500);
     });
-    search.on('click', e => {
-        $('.input-search-field').fadeToggle(500);
-        $(".input-discover-options").hide();
+    searchContainer.on('click', e => {
+        searchFields.fadeToggle(500);
+        discoverOpts.hide();
     });
 
+    //Grabbing the id of clicked btn
+    discoverOpts.on('click', '.discover-option', function() {
+        param = $(this).attr('value');
+        if (param == "primary_release_date.gte=") {param = `${param}${dateStamp}`}
+        else if(param == "primary_release_year=") {param = `${param}${year}`}
+        getResults(param);
+    });
+
+    //Initializing search and waiting for it to return a response
+    async function getResults(param) {
+        const Response = await fetchResults(param);
+        const Genres = await fetchGenres();
+
+        for(movie of Response.results) {
+            movieData.push({
+                'id': movie.id,
+                'genres': movie.genre_ids,
+                'title': movie.original_title,
+                'img': `${baseImgUrl}${movie.poster_path}`,
+                'release': movie.release_date
+            });
+        }
+
+        storage = JSON.stringify(movieData);
+        console.log(movieData);
+    }
+
     //Grabbing results based on search
-    async function fetchResults() {
+    async function fetchResults(param) {
         let result;
         try {
             result = await $.ajax({
-                url: apiUrl,
+                url: `${discoverUrl}${param}`,
                 type: 'GET'
             });
 
@@ -36,11 +67,22 @@ $(function () {
         }
     }
 
-    //Initializing search and waiting for it to return a response
-    async function getResults() {
-        const Response = await fetchResults();
-        console.log(Response);
+    async function fetchGenres() {
+        let result;
+        try {
+            result = await $.ajax({
+                url: genresUrl,
+                type: 'GET'
+            });
+            return result;
+        }
+        catch(error) {
+            console.error(error);
+        }
     }
 
+
+    //Setting this section dynamically - even though it will change once per year...
+    $('#best-movies-thisYear').html(`Best movies in ${year}`);
 
 }); //Main function fires when document loads
